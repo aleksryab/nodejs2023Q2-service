cb0024a742378f9e9f88f2c07c08ee368b46277e
aleksryab <aleksryabtsev@gmail.comm>
2023-07-27 18:55:57 +0700

    feat: set up project packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () =>
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      `Server started on http://localhost:${PORT}/`,
+    ),
+  );
 }
+
 bootstrap();
