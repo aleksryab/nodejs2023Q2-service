f2e4aab1cbcddd9581a29c4a6def2d95b7a8f10a
aleksryab <aleksryabtsev@gmail.comm>
2023-07-31 16:46:02 +0700

    feat: add swagger route message

--- src/utils/startServerMessage.ts
+++ src/utils/startServerMessage.ts
@@ -3,4 +3,9 @@ export function startServerMessage(port: string | number) {
     '\x1b[36m%s\x1b[0m',
     `Server started on http://localhost:${port}/`,
   );
+
+  console.log(
+    '\x1b[35m%s\x1b[0m',
+    `Swagger available on http://localhost:${port}/doc`,
+  );
 }
